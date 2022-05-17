a17b678936e5a29238e316176968998dd25bf7cc
Karunesh Kumar <59333448+Karunesh-Kumar1998@users.noreply.github.com>
2022-05-17 13:03:27 +0530

    Create github-script.sh

---
+++ github-script.sh
@@ -0,0 +1,16 @@
+ echo 1. What is in this directory?
+          ls -a
+          echo
+          echo 2. Is Java installed?
+          java -version
+          echo 
+          echo 3. Is Git install?
+          git --version
+          echo 4. What about build tools?
+          mvn --version
+          gradle --version
+          ant -version
+          echo 5. How is the disc laid out?
+          df
+          echo 6. What environment variables are avaialable?
+          env
